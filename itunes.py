b7de0a20163c53f1fdfc2b52c605c7b2509f98ac
cmoneylulz <element4emerica@gmail.com>
2014-07-17 18:18:27 +0400

    preparing to implement new parser

--- itunes.py
+++ itunes.py
@@ -1,25 +1,6 @@
-import xml.etree.ElementTree as ET
 
-class ITunesParser():
+
+class itunes():
   def __init__(self):
-    self.file_path = "/Users/cwilson/Music/iTunes/iTunes Music Library copy.xml"
+    self.file_path = "iTunes Music Library.xml"
 
-    print "Loading iTunes XML Library..."
-    self.etree = ET.parse(self.file_path)
-    self.root = self.etree.getroot()
-    print "Successfully Loaded XML Tree Root."
-
-    print "XML ROOT TAG: " + self.root.tag
-    print "XML ROOT ATTRIBUTES: "
-    print self.root.attrib
-
-    keys = self.root.find('dict')
-
-    tracklist = keys.find('dict')
-
-    count = 0
-    for track in tracklist:
-        print track.text
-    
-
-parser = ITunesParser()
